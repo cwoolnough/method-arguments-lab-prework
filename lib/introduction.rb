cf9c94f2cf8d98fbf24342eb7ffc3dbe5f1bf716
cwoolnoughchamorro@googlemail.com <cwoolnoughchamorro@googlemail.com>
2018-09-14 13:34:57 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,7 +5,7 @@ end
 introduction("Josh")
 
 def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and i am learning to programe in #{language}."
+  puts "Hi, my name is #{name} and i am learning to program in #{language}."
 end
 
 introduction_with_language("Dan", "Ember.js")
